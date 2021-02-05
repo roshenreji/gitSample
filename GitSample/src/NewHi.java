5df5a3bf8261c6be3d735acd563d1cda9554e51b
roshenreji <roshenreji.roshenreji@gmail.com>
2021-02-05 07:19:23 +0530

    third commit

---
+++ GitSample/src/NewHi.java
@@ -0,0 +1,12 @@
+
+public class NewHi {
+
+	public static void dance() {
+		System.out.println("Dance");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		dance();
+	}
+
+}
